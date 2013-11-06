fc395e38ccc7f694ee2e37c3c55a1f86e8b3434e
Nicklas Overgaard <nicklas@isharp.dk>
2013-11-06 19:47:29 +0100

    Added extension method .ToRollbar
    It extends the .net base Exception class and should
    thus enable users to run "exception.ToRollbar()" to add
    an exception, rather than the "(new RollbarClient).SendException"
    approach.

---
+++ src/RollbarSharp/ExceptionExtension.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace RollbarSharp
+{
+    /// <summary>
+    /// Provides extension methods for .net Exceptions
+    /// </summary>
+	public static class ExceptionExtension
+	{
+        /// <summary>
+        /// Adds the <see cref="Exception"/> instance to Rollbar
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+		public static void ToRollbar (this Exception exception)
+		{
+            if (exception != null)
+            {
+                (new RollbarClient()).SendException(exception);
+            }
+		}
+	}
+}
+
